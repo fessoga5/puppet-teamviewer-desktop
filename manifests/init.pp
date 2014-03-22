a2dc464d4316c5cba1eeefafd5ce1bc5e5072da8
root <root@d1stkfactory>
2014-03-22 08:04:14 +0000

    crete module for install teamviewer on debian whezzy

---
+++ manifests/init.pp
@@ -0,0 +1,24 @@
+# vim: sts=4 ts=4 sw=4 expandtab autoindent
+#
+#INSTALL SKYPE ON DESKTOP
+#
+class puppet-teamviewer-desktop ($source, $destination = "/root/teamviewer.deb") {
+    exec{ "dpkg --add-architecture i386;apt-get update;":
+        path => '/usr/bin:/bin:/usr/sbin:/sbin',
+        unless => '/usr/bin/dpkg --print-foreign-architectures | /bin/grep i386'
+    }
+
+    wget::fetch { 'teamviewer.deb':
+        source      => $source,
+        destination => $destination,
+        timeout     => 0,
+        verbose     => false,
+    }
+
+    package {"teamviewer":
+        provider => dpkg,
+        ensure   => latest,
+        source   => $destination,
+        require  => Wget::Fetch['teamviewer.deb']
+    }
+}
